fcfd490aa6d1a5d51c967dcc22091c680d07c9c0
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-17 09:08:46 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,84 @@
-fetch('https://dog.ceo/api/breeds/image/random/4')
+// fetch('https://dog.ceo/api/breeds/image/random/4')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+
+  fetch('https://dog.ceo/api/breeds/image/random/4')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => data.message.forEach(element => postDogs(element)))
+
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    breeds = Object.keys(data.message)
+    renderBreeds(breeds);
+    addBreedSelectListener();
+  })
+  
+})
+//data.message.forEach(element => renderBreeds(element)))
+
+//
+
+
+
+function postDogs(dogData) {
+  const dogImages = document.querySelector('#dog-image-container')
+  let dogImg = document.createElement('img')
+  dogImg.src = dogData
+  dogImg.style.height = '200px';
+  dogImg.style.padding = '2px';
+  dogImages.append(dogImg)  
+}
+
+
+
+function renderBreeds(breed) {
+  //debugger
+  
+
+  for (let i = 0; i < breed.length; i++) {
+    const breedUL = document.querySelector('#dog-breeds')
+    let breedLI = document.createElement('li')
+    breedLI.textContent = breed[i]
+    //debugger
+    breedUL.append(breedLI)
+    breedListener(breedLI)
+  }
+  
+}
+
+
+function breedListener(list) {
+  list.addEventListener('click', function(e) {
+    e.target.style.color = 'blue'
+  })
+}
+
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+
+  breedDropdown.addEventListener('change', function(e) {
+      startsWith(e.target.value)
+  })
+}
+
+
+function startsWith(letter) {
+  
+}
+
+
